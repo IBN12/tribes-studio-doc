08f931aef6e6b4c3e440d276db8349be4bb410be
IBN12 <IBN12@github.com>
2023-09-02 12:38:45 -0700

    Project links have been added to the navigation dropdown window.

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import { WorkspaceLogin } from './components/WorkspaceLogin';
 import { CloseButton } from './components/CloseButton';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 // workspace variant:
 const workspace ={
   hidden: {x: "100%"},
+  visible: {x: 0, opacity: 0.9 }
 }
 
 // navDropdown variant:
 const navDropdown = {
-  open: {x: [0, 150, 0], y: [0, 200, 0], scale: [1, 1, 1, 10, 1], borderRadius: [270, 270, 270, 0, 0], width: "100%", height: "100%", opacity: 0.8},
-  close: {x: 0},
+  open: {x: [0, 150, 0], y: [0, 200, 0], scale: [1, 1, 1, 10, 1], borderRadius: [270, 270, 270, 0, 0], width: "100%", height: "100%", opacity: 0.95},
+  close: {x: [0, 150, 0], y: [0, 200, 0], scale: [1, 10, 1, 1, 1], borderRadius: [0, 0, 270, 270, 270], width: "0", height: "0", opacity: 0.95},
 }
 
 // App(): The root component container.
@@ -25,27 +26,26 @@ const App = () => {
   const [displayNavDropdown, setDisplayNavDropdown] = useState(false);
   const [displayNavDropdownContent, setDisplayNavDropdownContent] = useState(false);
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      setDisplayNavDropdownContent(true);
-    }, 1200);
-  })
-
   function workspaceStation(e){
     console.log("Workspace Station");
     console.log(e.target);
     setDisplayWorkspaceWindow(true);
   }
 
-  function handleClick(){
+  function navigationDropdown(){
     console.log("Nav Button Clicked!");
+
+    setTimeout(()=>{
+      setDisplayNavDropdownContent(true);
+    }, 1000);
+
     setDisplayNavDropdown(true);
   }
 
   return (
     <div className="app-component-container">
       <div>
-        <button onClick={handleClick}>
+        <button onClick={navigationDropdown}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
             </svg>
@@ -67,7 +67,21 @@ const App = () => {
             {displayNavDropdown ? 
               <>
                 {displayNavDropdownContent ?
-                  <CloseButton />
+                  <>
+                    <CloseButton
+                      displayWorkspaceWindow={displayWorkspaceWindow}
+                      displayNavDropdown={displayNavDropdown}
+                      setDisplayWorkspaceWindow={setDisplayWorkspaceWindow}
+                      setDisplayNavDropdown={setDisplayNavDropdown}
+                      setDisplayNavDropdownContent={setDisplayNavDropdownContent}
+                    />
+                    <div className="nav-dropdown-content">
+                      <h2>Projects</h2>
+                      <Link to="projects/The Electronic One">The Electronic One</Link>
+                      <Link to="projects/Strive">Strive</Link>
+                      <Link to="projects/Cerebral">CEREBRAL: THE FINAL HOPE</Link>
+                    </div>
+                  </>
                   :
                   null
                 }
@@ -81,13 +95,21 @@ const App = () => {
         className="workspace-window-component-container"
         variants={workspace}
         initial="hidden"
-        animate={{x: displayWorkspaceWindow ? 0 : "100%"}}
+        animate={displayWorkspaceWindow ?
+          "visible"
+          :
+          "hidden"
+        }
         transition={{duration: 0.8, ease: "easeIn", type: "spring", bounce: 0.3}}>
           
           {displayWorkspaceWindow  && (
             <>
               <CloseButton 
+                displayWorkspaceWindow={displayWorkspaceWindow}
+                displayNavDropdown={displayNavDropdown}
                 setDisplayWorkspaceWindow={setDisplayWorkspaceWindow}
+                setDisplayNavDropdown={setDisplayNavDropdown}
+                setDisplayNavDropdownContent={setDisplayNavDropdownContent}
               />
               <WorkspaceLogin/>
             </>
--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -4,11 +4,27 @@ import PropTypes from 'prop-types';
 
 //CloseButton(): The close button component container.
 export const CloseButton = (props) => {
-    const {setDisplayWorkspaceWindow} = props;
+    const {
+        displayWorkspaceWindow, 
+        displayNavDropdown, 
+        setDisplayWorkspaceWindow, 
+        setDisplayNavDropdown, 
+        setDisplayNavDropdownContent} = props;
 
     return (
         <div className="close-button-component-container">
-            <button onClick={() => setDisplayWorkspaceWindow(false)}>
+            <button onClick={() => {
+                if (displayWorkspaceWindow)
+                {
+                    setDisplayWorkspaceWindow(false);
+                }
+                else if(displayNavDropdown)
+                {
+                    setDisplayNavDropdown(false);
+                    setDisplayNavDropdownContent(false);
+                }
+                
+            }}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
                 </svg>
@@ -20,4 +36,8 @@ export const CloseButton = (props) => {
 // CloseButton PropType: 
 CloseButton.propTypes = {
     setDisplayWorkspaceWindow: PropTypes.func,
+    displayWorkspaceWindow: PropTypes.bool,
+    displayNavDropdown: PropTypes.bool,
+    setDisplayNavDropdown: PropTypes.func,
+    setDisplayNavDropdownContent: PropTypes.func,
 }
